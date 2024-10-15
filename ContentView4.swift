e1753bf516de511745bf49c6681c223eb43bc141
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-10-15 10:50:12 +0800

    Update ContentView4.swift

--- ContentView4.swift
+++ ContentView4.swift
@@ -1,74 +1,74 @@
-//import SwiftUI
-//
-//struct ContentView: View {
-//    var body: some View {
-//        VStack {
-//            HeaderView()
-//            
-//            HStack {
-//                PricingView(title: "Basic", price: "$9", textColor: .white, bgColor: .purple)
-//                
-//                ZStack {
-//                    PricingView(title: "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
-//                    
-//                    Text("Best for designer")
-//                        .font(.system(.caption, design: .rounded))
-//                        .fontWeight(.bold)
-//                        .foregroundColor(.white)
-//                        .padding(5)
-//                        .background(Color(red: 255/255, green: 183/255, blue: 37/255))
-//                        .offset(x: 0, y: 87)
-//                }
-//            }
-//            .padding(.horizontal)
-//        }
-//    }
-//}
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
-//
-//struct HeaderView: View {
-//    var body: some View {
-//        VStack(alignment: .leading, spacing: 2) {
-//            Text("Choose")
-//                .font(.system(.largeTitle, design: .rounded))
-//                .fontWeight(.black)
-//            Text("Your Plan")
-//                .font(.system(.largeTitle, design: .rounded))
-//                .fontWeight(.black)
-//        }
-//    }
-//}
-//
-//struct PricingView: View {
-//    var title: String
-//    var price: String
-//    var textColor: Color
-//    var bgColor: Color
-//    
-//    var body: some View {
-//        VStack {
-//            Text(title)
-//                .font(.system(.title, design: .rounded))
-//                .fontWeight(.black)
-//                .foregroundColor(textColor)
-//            Text(price)
-//                .font(.system(size: 40, weight: .heavy, design: .rounded))
-//                .foregroundColor(textColor)
-//            Text("per month")
-//                .font(.headline)
-//                .foregroundColor(textColor)
-//        }
-//        .padding(40)
-//        .background(bgColor)
-//        .cornerRadius(10)
-//    }
-//}
-//
-//#Preview {
-//    ContentView()
-//}
+import SwiftUI
+
+struct ContentView: View {
+   var body: some View {
+       VStack {
+           HeaderView()
+           
+           HStack {
+               PricingView(title: "Basic", price: "$9", textColor: .white, bgColor: .purple)
+               
+               ZStack {
+                   PricingView(title: "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
+                   
+                   Text("Best for designer")
+                       .font(.system(.caption, design: .rounded))
+                       .fontWeight(.bold)
+                       .foregroundColor(.white)
+                       .padding(5)
+                       .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                       .offset(x: 0, y: 87)
+               }
+           }
+           .padding(.horizontal)
+       }
+   }
+}
+
+struct ContentView_Previews: PreviewProvider {
+   static var previews: some View {
+       ContentView()
+   }
+}
+
+struct HeaderView: View {
+   var body: some View {
+       VStack(alignment: .leading, spacing: 2) {
+           Text("Choose")
+               .font(.system(.largeTitle, design: .rounded))
+               .fontWeight(.black)
+           Text("Your Plan")
+               .font(.system(.largeTitle, design: .rounded))
+               .fontWeight(.black)
+       }
+   }
+}
+
+struct PricingView: View {
+   var title: String
+   var price: String
+   var textColor: Color
+   var bgColor: Color
+   
+   var body: some View {
+       VStack {
+           Text(title)
+               .font(.system(.title, design: .rounded))
+               .fontWeight(.black)
+               .foregroundColor(textColor)
+           Text(price)
+               .font(.system(size: 40, weight: .heavy, design: .rounded))
+               .foregroundColor(textColor)
+           Text("per month")
+               .font(.headline)
+               .foregroundColor(textColor)
+       }
+       .padding(40)
+       .background(bgColor)
+       .cornerRadius(10)
+   }
+}
+
+#Preview {
+   ContentView()
+}
